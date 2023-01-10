6d0564baf1fe4503c810a62f429a87afeab1ec16
Neeti Kumar <121576070+neeti-nk47@users.noreply.github.com>
2023-01-11 00:38:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello")
+class Student{
+    static counter=0;
+  
+    constructor(name ,age , boardmarks){
+        this.name=name, 
+        this.age=age, 
+        this.boardmarks=boardmarks,
+        Student.counter++
+    }
+    checkEligible= function() {
+        if (this.boardmarks>40) console.log("Eligible");
+
+        else console.log("not eligible");
+    }
+
+}
+
+var ram=new Student("ram",21,83);
+var shyam=new Student("shyam",52,32);
+var yam=new Student("yam",13,42);
+var tom=new Student("tom",72,92);
+var bill=new Student("bill",22,25);
+console.log(Student.counter)
+
+shyam.checkEligible()
