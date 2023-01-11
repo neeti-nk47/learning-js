3d3aee76d2a32bd6c4e9e91988d17d78be4fcdd7
Neeti Kumar <121576070+neeti-nk47@users.noreply.github.com>
2023-01-12 00:55:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
-class Student{
-    static counter=0;
-    constructor(name ,age , boardmarks){
-        this.name=name, 
-        this.age=age, 
-        this.boardmarks=boardmarks,
-        Student.counter++
-    }
+function attachEventListeners(){
+    let button= document.getElementById("clickme");
+
+    addEventListener('DOMContentLoaded',function dom(){
+        console.log("DOM has loaded");
+    });
     
-    eligibleForPlacement= function(minMarks){
-        return (minAge) =>{
-            if(this.age>minAge && this.boardmarks>minMarks){
-                console.log(this.name + " is eligible for placements");
-            }
-            else console.log(this.name + " is not eligible for placements");
-        }
-    }
+    button.addEventListener("click",function clicked(){
+        console.log("clicked me");
+    });
+
 }
 
-var ram=new Student("ram",21,83);
-var shyam=new Student("shyam",52,32);
-var yam=new Student("yam",13,42);
-var tom=new Student("tom",72,92);
-var bill=new Student("bill",22,25);
+attachEventListeners();
 
-console.log(Student.counter)
 
-tom.eligibleForPlacement(40)(21)
+
+
